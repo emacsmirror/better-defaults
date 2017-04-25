6f502fd2d7aa8940a31fd630f883a63e04a002e6
Phil Hagelberg <phil@hagelb.org>
2017-04-25 08:34:08 -0700

    Remove unnecessary x-select-* settings.
    Fixes #22.

--- better-defaults.el
+++ better-defaults.el
@@ -75,9 +75,7 @@
 
   (show-paren-mode 1)
   (setq-default indent-tabs-mode nil)
-  (setq x-select-enable-clipboard t
-        x-select-enable-primary t
-        save-interprogram-paste-before-kill t
+  (setq save-interprogram-paste-before-kill t
         apropos-do-all t
         mouse-yank-at-point t
         require-final-newline t
